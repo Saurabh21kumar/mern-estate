8de7fc9f58a13bb40c011a4b3807fd4d8dc41402
saurabh-dell <saurabhkumar52940@gmail.com>
2024-04-20 21:34:05 +0530

    ...

--- api/index.js
+++ api/index.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
-// import cors from "cors";
-
+import path from 'path';
 dotenv.config();
+
+
 
 mongoose.connect(process.env.DATABASE_URI).then(() =>{
     console.log("Connected to Database!");
@@ -15,6 +16,7 @@ mongoose.connect(process.env.DATABASE_URI).then(() =>{
     console.log(err);
 });
 
+const __dirname = path.resolve();
 
 const app=express();
 
@@ -35,6 +37,12 @@ app.get('/',(req, res)=>{
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) =>{
     const statusCode=err.statusCode || 500;
