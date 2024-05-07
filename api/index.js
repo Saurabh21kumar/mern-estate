b7086e6d412c243054761f01a427bd3f8e466f9c
saurabh-dell <saurabhkumar52940@gmail.com>
2024-05-07 20:47:43 +0530

    project end

--- api/index.js
+++ api/index.js
@@ -8,48 +8,44 @@ import cookieParser from 'cookie-parser';
 import path from 'path';
 dotenv.config();
 
-
-
-mongoose.connect(process.env.DATABASE_URI).then(() =>{
-    console.log("Connected to Database!");
-}).catch((err) => {
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
-const __dirname = path.resolve();
+  const __dirname = path.resolve();
 
-const app=express();
-
-// app.use(cors())
+const app = express();
 
 app.use(express.json());
 
 app.use(cookieParser());
-  
-app.listen(8000, ()=>{
-    console.log("Server is running!! ");
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
 
-app.get('/',(req, res)=>{
-    res.send("Hi I am saurabh kumar!");
-});
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
-app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/listing", listingRouter);
 
-// app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-// })
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
-app.use((err, req, res, next) =>{
-    const statusCode=err.statusCode || 500;
-    const message=err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    });
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
